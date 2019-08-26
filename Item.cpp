1f373bf7017d12729b228977649a9bd972ad4c1c
nsakavya1804 <46874626+nsakavya1804@users.noreply.github.com>
2019-08-25 19:12:46 -0700

    Create Item.cpp

---
+++ Item.cpp
@@ -0,0 +1,21 @@
+#ifndef ITEM_H
+#define ITEM_H
+#include <string>
+
+class Item {
+   private:
+       std::string name;
+       double price;
+
+   public:
+       Item();
+       Item(std::string str, double prc);
+       void setName(std::string newName);
+       void setPrice(double price);
+       std::string getName();
+       double getPrice();
+       void printItem();
+       bool operator==(Item const rhs);
+};
+
+#endif
