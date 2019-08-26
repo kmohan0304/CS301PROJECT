e721c6186a77ce5799c9b9371969d8ae63fd9259
nsakavya1804 <46874626+nsakavya1804@users.noreply.github.com>
2019-08-25 21:35:18 -0700

    Create OrderQueue.h

---
+++ OrderQueue.h
@@ -0,0 +1,34 @@
+#ifndef ORDERQUEUE_H
+#define ORDERQUEUE_H
+#include "Order.h"
+
+struct OrderList {
+   Order data;
+   OrderList *next;
+};
+
+class OrderQueue {
+   private:
+       OrderList *orders; // front
+       OrderList *back; // back
+       int latestOrderNumber;
+       int maxSize;
+       int size;
+       int currentOrder;
+
+   public:
+       OrderQueue();
+       OrderQueue(int queueSize);
+       ~OrderQueue();
+       int     issueOrderNumber();
+       int     getSize();
+       void    addOrder(Order &addMe); // enqueue
+       void    removeAllOrders(); // O(n)
+       void    printOrders(); // O(n)
+       Order*  completeOrder(); // dequeue
+       Order*  completeOrder(int indexToDelete); // O(n)
+       Order   getCurrentOrder(); // front
+       Order   getRecentOrder(); // back
+};
+
+#endif
