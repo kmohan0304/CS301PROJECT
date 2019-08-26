0865c768edf1894b3c206d277a329c3b6f6ff054
nsakavya1804 <46874626+nsakavya1804@users.noreply.github.com>
2019-08-25 19:17:35 -0700

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -1,21 +1,42 @@
-#ifndef ITEM_H
-#define ITEM_H
-#include <string>
+#include "Item.h"
+#include <iostream>
+#include <iomanip>
 
-class Item {
-   private:
-       std::string name;
-       double price;
+// Constructors
+Item::Item() { // Default constructor
+    name = " "; // set a generic name and price of $0.00
+    price = 0;
+}
 
-   public:
-       Item();
-       Item(std::string str, double prc);
-       void setName(std::string newName);
-       void setPrice(double price);
-       std::string getName();
-       double getPrice();
-       void printItem();
-       bool operator==(Item const rhs);
-};
+Item::Item(std::string str, double prc) { // normal constructor
+    name = str;
+    price = prc;
+}
 
-#endif
+void Item::setName(std::string newName) {
+    name = newName;
+}
+
+void Item::setPrice(double newPrice) {
+    price = newPrice;
+}
+
+std::string Item::getName() {
+    return name;
+}
+
+double Item::getPrice() {
+    return price;
+}
+
+void Item::printItem() {
+    std::cout << std::fixed << std::setprecision(2)
+             << name << ": $" << price << std::endl;
+}
+
+bool Item::operator==(Item const rhs) {
+    if (name == rhs.name && price == rhs.price) {
+        return true;
+    }
+    return false;
+}
